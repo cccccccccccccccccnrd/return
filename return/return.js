2888454a42ebae8705469474430d4b4dfbefcc58
Kjell <kjell.xvx@t-online.de>
2022-04-05 14:58:42 +0200

    added collapsibles

--- return/return.js
+++ return/return.js
@@ -115,3 +115,21 @@ async function init() {
 }
 
 init()
+
+
+var coll = document.getElementsByClassName('collapsible')
+var i
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener('click', function () {
+    this.classList.toggle('active')
+    var content = this.nextElementSibling
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null
+      content.style.margin = "0 0 0 0"
+    } else {
+      content.style.maxHeight = "none"
+      content.style.margin = "0.5em 0 0 0"
+    }
+  })
+}
